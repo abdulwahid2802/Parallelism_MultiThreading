7eae15e34bbb5b44a54e4b23ec3f1ac665170797
abdulwahid2802 <wakhid2802@gmail.com>
2018-09-09 23:23:01 +0900

    Wait some time before cacelling

--- Parallellism-1.TaskCreating/Program.cs
+++ Parallellism-1.TaskCreating/Program.cs
@@ -10,84 +10,21 @@ namespace Parallellism1.TaskCreating
         {
 
 
-            //+++++ Cancelling tasks +++++++//
+            //+++++ Waiting some time +++++++//
 
-            // Cancellation token to pass into task
             var cts = new CancellationTokenSource();
             var token = cts.Token;
 
-
-            // Subscribe to token to wait for cancellations to get notified
-            token.Register(()=>
+            Task.Factory.StartNew(() =>
             {
-                Console.WriteLine("Cancellation has been requested...");
-            });
+                Console.WriteLine("You have 5 seconds to disarm the bomb!");
 
-            var task = new Task(
-                ()=>
-            {
-                for (int i = 0; ; i++)
-                {
-
-                    //+++++ METHOD 1 of cancelling +++++//
-                    if (token.IsCancellationRequested)
-                        throw new OperationCanceledException();
-                    else
-                        Console.WriteLine(i);
-
-                    //+++++ METHOD 2 of cancelling +++++//
-                    // merges 'if and throw' statements
-                    //token.ThrowIfCancellationRequested();
-                    //Console.WriteLine(i);
-                }
+                Console.WriteLine(token.WaitHandle.WaitOne(5000) ? "You have disarmed..." : "BOOM!!!");
             }, token);
 
-            task.Start();
 
-            Task.Factory.StartNew(() =>
-            {
-                token.WaitHandle.WaitOne();
-                Console.WriteLine("Wait handle has been released, cancellation requested...");
-            });
-
-            // Sends the cancellation request when key pressed
             Console.ReadKey();
             cts.Cancel();
-
-
-
-            //+++++ Complex Tokens +++++++//
-
-
-            var planned = new CancellationTokenSource();
-            var preventative = new CancellationTokenSource();
-            var emergency = new CancellationTokenSource();
-
-            // Links the tokens
-            var paranoid = CancellationTokenSource.CreateLinkedTokenSource
-                                                  (
-                                                      planned.Token,
-                                                      preventative.Token,
-                                                      emergency.Token
-                                                     );
-
-            Task.Factory.StartNew(() =>
-            {
-                for (int i = 0; ; i++)
-                {
-                    paranoid.Token.ThrowIfCancellationRequested();
-                    Console.WriteLine($"{i}");
-                    Thread.Sleep(1000);
-                }
-            });
-
-
-            Console.ReadKey();
-            // now we can call cancellation using any token
-            emergency.Cancel();
-
-
-
 
         }
     }
