f64b1839d06db46e6c8b6624565a5f9fda0911e3
abdulwahid2802 <wakhid2802@gmail.com>
2018-09-10 18:37:53 +0900

    Exception Handling in C#

--- Parallellism-1.TaskCreating/Program.cs
+++ Parallellism-1.TaskCreating/Program.cs
@@ -10,46 +10,31 @@ namespace Parallellism1.TaskCreating
         {
 
 
-            //+++++ Waiting for tasks +++++++//
+            //+++++ Exception Handling +++++++//
 
-            var cts = new CancellationTokenSource();
-            var token = cts.Token;
-
-            var task_1 = new Task(() =>
+            var task_1 = Task.Factory.StartNew(() =>
             {
-                for (int i = 0; i < 5;i++)
-                {
-                    token.ThrowIfCancellationRequested();
-                    Thread.Sleep(1000);
-                }
-                Console.WriteLine("Task 1 is done!");
-            }, token);
-            task_1.Start();
+                throw new AccessViolationException("You do not have permission!") { Source = "task_1" };
+            });
 
-            var task_2 = new Task(() =>
+            var task_2 = Task.Factory.StartNew(() =>
             {
-                for (int i = 0; i < 3; i++)
+                throw new InvalidOperationException("You cannot do this!") { Source = "task_2" };
+            });
+
+
+            try
+            {
+                Task.WaitAll(task_1, task_2);
+            }
+            catch(AggregateException ae)
+            {
+                foreach(var e in ae.InnerExceptions)
                 {
-                    token.ThrowIfCancellationRequested();
-                    Thread.Sleep(1000);
+                    Console.WriteLine($"Exception {e.GetType()} from {e.Source} has been thrown...");
                 }
-                Console.WriteLine("Task 2 is done!");
-            }, token);
-            task_2.Start();
+            }
 
-            // .WaitAny() waits for any thread  that is passed to it to finish
-            //Task.WaitAny(task_1, task_2);
-
-            // .WaitAll() waits for the longest thread to finish
-            //Task.WaitAll(task_1, task_2);
-
-
-            // pass multiple tasks as an array and a time to wait for
-            Task.WaitAny(new[] { task_1, task_2 }, 4000, token);
-
-            // Lets look at the thread status
-            Console.WriteLine($"Task 1 status is {task_1.Status}");
-            Console.WriteLine($"Task 2 status is {task_2.Status}");
 
             Console.WriteLine("Main Thread is done!");
 
