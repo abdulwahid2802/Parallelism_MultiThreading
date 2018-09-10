1abf36ac268b625da6b3de7ddd0434089017fadf
abdulwahid2802 <wakhid2802@gmail.com>
2018-09-10 16:10:25 +0900

    Waiting for the tasks to finish...

--- Parallellism-1.TaskCreating/Program.cs
+++ Parallellism-1.TaskCreating/Program.cs
@@ -10,21 +10,48 @@ namespace Parallellism1.TaskCreating
         {
 
 
-            //+++++ Waiting some time +++++++//
+            //+++++ Waiting for tasks +++++++//
 
             var cts = new CancellationTokenSource();
             var token = cts.Token;
 
-            Task.Factory.StartNew(() =>
+            var task_1 = new Task(() =>
             {
-                Console.WriteLine("You have 5 seconds to disarm the bomb!");
-
-                Console.WriteLine(token.WaitHandle.WaitOne(5000) ? "You have disarmed..." : "BOOM!!!");
+                for (int i = 0; i < 5;i++)
+                {
+                    token.ThrowIfCancellationRequested();
+                    Thread.Sleep(1000);
+                }
+                Console.WriteLine("Task 1 is done!");
             }, token);
+            task_1.Start();
+
+            var task_2 = new Task(() =>
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    token.ThrowIfCancellationRequested();
+                    Thread.Sleep(1000);
+                }
+                Console.WriteLine("Task 2 is done!");
+            }, token);
+            task_2.Start();
+
+            // .WaitAny() waits for any thread  that is passed to it to finish
+            //Task.WaitAny(task_1, task_2);
+
+            // .WaitAll() waits for the longest thread to finish
+            //Task.WaitAll(task_1, task_2);
 
 
-            Console.ReadKey();
-            cts.Cancel();
+            // pass multiple tasks as an array and a time to wait for
+            Task.WaitAny(new[] { task_1, task_2 }, 4000, token);
+
+            // Lets look at the thread status
+            Console.WriteLine($"Task 1 status is {task_1.Status}");
+            Console.WriteLine($"Task 2 status is {task_2.Status}");
+
+            Console.WriteLine("Main Thread is done!");
 
         }
     }
